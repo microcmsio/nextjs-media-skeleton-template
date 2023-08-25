4e3ca84ac5cf8611df46bd59bdc4e77f78fecf12
Kazuki Shibata <shibata@microcms.co.jp>
2023-08-25 11:12:59 +0900

    [update] adを一旦削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import Script from 'next/script';
+// import Script from 'next/script';
 import './globals.css';
 import Header from '@/_components/Header';
 import { GTM } from '@/_components/GTM';
@@ -14,16 +14,14 @@ const isProduction: boolean = process.env.NODE_ENV === 'production';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      {isProduction && (
-        <>
-          <Script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1850426805870963"
-            crossOrigin="anonymous"
-          />
-          <GTM />
-        </>
-      )}
+      {/* {isProduction && (
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1850426805870963"
+          crossOrigin="anonymous"
+        />
+      )} */}
+      {isProduction && <GTM />}
       <body>
         <Header />
         {children}
