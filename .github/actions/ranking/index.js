5f25745c70669da25db3af2d9b4900547b52f84d
Kazuki Shibata <shibata@microcms.co.jp>
2023-08-25 13:52:07 +0900

    [update] ランキングをGitHub Actionsで動かす

---
+++ .github/actions/ranking/index.js
@@ -0,0 +1,64 @@
+import { BetaAnalyticsDataClient } from '@google-analytics/data';
+import { createClient } from 'microcms-js-sdk';
+const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+const serviceAccountKey = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY || '');
+const analyticsDataClient = new BetaAnalyticsDataClient({
+  credentials: {
+    client_email: serviceAccountKey.client_email,
+    private_key: serviceAccountKey.private_key,
+  },
+});
+
+export const getPopularArticles = async () => {
+  const [response] = await analyticsDataClient.runReport({
+    property: `properties/${process.env.GA_PROPERTY_ID}`,
+    dateRanges: [
+      {
+        startDate: '8daysAgo',
+        endDate: '1daysAgo',
+      },
+    ],
+    dimensions: [
+      {
+        name: 'pagePath',
+      },
+    ],
+    metrics: [
+      {
+        name: 'screenPageViews',
+      },
+    ],
+    // トップページ（/）を除く
+    dimensionFilter: {
+      filter: {
+        fieldName: 'pagePath',
+        stringFilter: {
+          matchType: 'FULL_REGEXP',
+          value: '^/articles/[^/]+$',
+        },
+      },
+    },
+    limit: '5',
+  });
+  const data = response.rows?.map((row) => {
+    return {
+      path: row.dimensionValues?.[0].value,
+      views: row.metricValues?.[0].value,
+    };
+  });
+  return data;
+};
+
+const popularArticles = (await getPopularArticles()) || [];
+const ids = popularArticles.map((article) => article.path.slice(1));
+
+await client.update({
+  endpoint: 'ranking',
+  content: {
+    articles: ids,
+  },
+});
